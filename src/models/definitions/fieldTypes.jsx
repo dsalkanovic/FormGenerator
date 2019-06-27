daf1c7c2d5677f9f84bc5ef7a9f44862d9d3f4be
Denis Salkanovic <dsalkanovic@gmail.com>
2019-06-27 18:38:34 +0200

    label

--- src/models/definitions/fieldTypes.jsx
+++ src/models/definitions/fieldTypes.jsx
@@ -1,36 +1,42 @@
 import { TextFieldDefinition } from './text';
 
 export const FiledTypes = {
-    Page: { name: 'FT_PAGE', labe: 'Page', icon: 'File', rank: 1000 },
-    Group: { name: 'FT_GROUP', labe: 'Group', icon: 'Bookmark', rank: 900 },
-    Text: { name: 'FT_TEXT', labe: 'Text', buildDefinition: d => new TextFieldDefinition(d), icon: 'Type', rank: 100 },
+    Page: { name: 'FT_PAGE', label: 'Page', icon: 'File', rank: 1000 },
+    Group: { name: 'FT_GROUP', label: 'Group', icon: 'Bookmark', rank: 900 },
+    Text: { name: 'FT_TEXT', label: 'Text', buildDefinition: d => new TextFieldDefinition(d), icon: 'Type', rank: 100 },
     Number: {
         name: 'FT_NUMBER',
-        labe: 'Number',
+        label: 'Number',
         buildDefinition: d => new TextFieldDefinition(d),
         icon: 'Hash',
         rank: 100
     },
     Select: {
         name: 'FT_SELECT',
-        labe: 'Select',
+        label: 'Select',
         buildDefinition: d => new TextFieldDefinition(d),
         icon: 'List',
         rank: 100
     },
     Boolean: {
         name: 'FT_BOOLEAN',
-        labe: 'Checkbox',
+        label: 'Checkbox',
         buildDefinition: d => new TextFieldDefinition(d),
         icon: 'ToggleLeft',
         rank: 100
     },
     Date: {
         name: 'FT_DATE',
-        labe: 'Date',
+        label: 'Date',
         buildDefinition: d => new TextFieldDefinition(d),
         icon: 'Calendar',
         rank: 100
     },
-    File: { name: 'FT_FILE', labe: 'File', buildDefinition: d => new TextFieldDefinition(d), icon: 'Upload', rank: 100 }
+    File: {
+        name: 'FT_FILE',
+        label: 'File',
+        buildDefinition: d => new TextFieldDefinition(d),
+        icon: 'Upload',
+        rank: 100
+    }
 };
