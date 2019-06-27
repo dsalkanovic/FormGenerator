f545a41825932aae8798513f1f0f7db663b0812d
Denis Salkanovic <dsalkanovic@gmail.com>
2019-06-27 18:33:36 +0200

    panes

--- src/containers/configurator/components/properties/index.jsx
+++ src/containers/configurator/components/properties/index.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Card, Elevation } from '@blueprintjs/core';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Card } from '@blueprintjs/core';
+import SimpleBar from 'simplebar-react';
 
 class Properties extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
     render() {
+        const { height, selected = {} } = this.props;
+        const { page, group, field } = selected;
+
         return (
-            <div className="configurator-properties">
-                <Card elevation={Elevation.ONE}>
-                    <h4 className="configurator-pane-title">properties</h4>
+            <div className="configurator-properties" style={{ height: `${height - 70}px` }}>
+                <Card elevation={1}>
+                    <Card elevation={0} className="fg-panel-header">
+                        <div className="fg-panel-title">
+                            <span className="pd-l-5">Properties</span>
+                        </div>
+                    </Card>
+                    <SimpleBar style={{ height: `${height - 121}px` }}>
+                        <div className="pd-20">
+                            <p>{JSON.stringify(page)}</p>
+                            <p>{JSON.stringify(group)}</p>
+                            <p>{JSON.stringify(field)}</p>
+                        </div>
+                    </SimpleBar>
                 </Card>
             </div>
         );
     }
 }
 
-export default Properties;
+const mapStateToProps = state => ({
+    selected: state.configurator.selected
+});
+const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Properties);
